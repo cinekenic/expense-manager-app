96d250b592d79782e725a8809d9aea1b940305f5
Piotr-Trzcinski <cinekenic@go2.pl>
2025-07-14 18:45:07 +0200

    integrated react router

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,12 @@
 /** @format */
+import { Link } from "react-router-dom";
 import wallet from "../assets/wallet.png";
 
 const Logo = () => {
   return (
-    <a className="navbar-brand" href="#">
+    <Link className="navbar-brand" to="/">
       <img src={wallet} alt="Logo" width={48} height={48} className="rounded-circle bg-light p-1" />
-    </a>
+    </Link>
   );
 };
 
