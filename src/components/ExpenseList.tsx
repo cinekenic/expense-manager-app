cb76b072ae5cffaf561bf0d3cafe17a6efdffe58
Piotr-Trzcinski <cinekenic@go2.pl>
2025-07-14 22:46:14 +0200

    completed the dashboard status

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,6 +1,8 @@
 /** @format */
 
 import type { Expense } from "../model/Expense";
+import CurrencyUtils from "../utils/CurrencyUtils";
+import DateUtils from "../utils/DateUtils";
 
 interface Props {
   expenses: Expense[];
@@ -19,10 +21,12 @@ const ExpenseList = ({ expenses }: Props) => {
             <div className="d-flex justify-content-between border-bottom-1 p-3 text-dark">
               <div className="card-title m-0">
                 <h5>{expense.name}</h5>
-                <span className="fst-italic">{new Date(expense.date).toLocaleDateString("pl-PL")}</span>
+                <span className="fst-italic">{DateUtils.formatDateString(expense.date)}</span>
               </div>
               <div className="card-subtitle">
-                <span className="badge rounded-pill app-primary-bg-color">{expense.amount}</span>
+                <span className="badge rounded-pill app-primary-bg-color">
+                  {CurrencyUtils.formatToPLN(expense.amount)}
+                </span>
               </div>
             </div>
           </div>
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -2,14 +2,22 @@
 
 import ExpenseList from "../../components/ExpenseList";
 import useExpenses from "../../hooks/useExpenses";
+import type { Expense } from "../../model/Expense";
+import DashboardStatus from "./DashboardStatus";
 
 const Dashboard = () => {
+  const loggedInUser: string = "John Doe";
+
   const { expenses, error, loading } = useExpenses();
+
+  const totalExpenses = expenses.reduce((total: number, expense: Expense) => total + expense.amount, 0);
 
   return (
     <div className="container">
       {loading && <div>Loading...</div>}
       {error && <div>Error: {error}</div>}
+      <DashboardStatus loggedInUser={loggedInUser} totalExpenses={totalExpenses} />
+      <hr />
       <ExpenseList expenses={expenses} />
     </div>
   );
