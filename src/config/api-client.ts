37b16b1508cbf68eecaaceab48aaabea6c445a60
Piotr-Trzcinski <cinekenic@go2.pl>
2025-08-03 22:31:00 +0200

    added apiClient

--- src/config/api-client.ts
+++ src/config/api-client.ts
@@ -2,6 +2,10 @@
 
 import axios from "axios";
 
+// const apiClient = axios.create({
+//   baseURL: "http://localhost:8080/api/v1",
+// });
+
 const apiClient = axios.create({
   baseURL: "https://expense-manager-restapi.onrender.com/api/v1",
 });
