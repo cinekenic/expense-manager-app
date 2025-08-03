39b543fc4cd516a6c349f445ba1e401c2c4fd381
Piotr-Trzcinski <cinekenic@go2.pl>
2025-08-04 00:30:25 +0200

    added config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   base: "/expense-manager-app/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    open: true,
+  },
 });
